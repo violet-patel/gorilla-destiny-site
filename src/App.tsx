e55fc5ed22e2727deb76f73eacbabbadace6c065
Violet Patel <79294780+violet-patel@users.noreply.github.com>
2025-07-09 16:20:16 -0400

    Added font, logo, and button bar

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,19 @@ import './App.css'
 function App() {
   return (
     <>
-      <div>
+      <div className="header">
+        <h1 className="unifrakturmaguntia-regular text-6xl text-center">
+          Gorilla Destiny
+        </h1>
+        <hr></hr>
+        <div className="button_bar space-x-3 py-2">
+          <button className="button">Home</button>
+          <button className="button">Shows</button>
+          <button className="button">Gallery</button>
+          <button className="button">Contact</button>
+        </div>
+      </div>
+      <div className="content_container">
         <a href="https://www.instagram.com/gorilladestinyband/" target="_blank">
           <img src={bandLogo} className="logo w-128 h-128" alt="Gorilla Destiny logo" />
         </a>
