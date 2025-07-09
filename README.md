fa9925889652c7c8b65af9583924523c622aa534
Violet Patel <79294780+violet-patel@users.noreply.github.com>
2025-07-09 05:45:22 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Gorilla Destiny Website
+Official band website for Gorilla Destiny, built with React and TailwindCSS
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import bandLogo from './assets/logo.webp'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://www.instagram.com/gorilladestinyband/" target="_blank">
+          <img src={bandLogo} className="logo w-128 h-128" alt="Gorilla Destiny logo" />
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default App
